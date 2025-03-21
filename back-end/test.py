632a1e145733eb9b259eceb703abd71146c1bc41
OwenC <66129237+Fxreball@users.noreply.github.com>
2025-03-21 17:35:10 +0100

    Overbodige files verwijderd.

--- back-end/test.py
+++
@@ -1,9 +0,0 @@
-from rapidfuzz import fuzz
-
-title_1 = "The Substance"
-title_2 = "Substance, The"
-title_3 = "Wicked (NL)"
-title_4 = "Wicked"
-
-print("The Substance vs Substance, The:", fuzz.token_sort_ratio(title_1, title_2), "%")
-print("Wicked (NL) vs Wicked:", fuzz.token_sort_ratio(title_3, title_4), "%")
